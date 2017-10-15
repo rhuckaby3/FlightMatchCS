0fff9483021b5d3b30cd9c3b4a5039dc6ffda87c
Richard Curtis Huckaby <rhuckaby3@gatech.edu>
2017-10-15 08:08:14 -0400

    initial commit

---
+++ DeltaProfile/Form1.Designer.cs
@@ -0,0 +1,344 @@
+﻿namespace DeltaProfile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.radioButton9 = new System.Windows.Forms.RadioButton();
+            this.radioButton10 = new System.Windows.Forms.RadioButton();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
+            this.radioButton8 = new System.Windows.Forms.RadioButton();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox5.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(13, 23);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(970, 781);
+            this.tabControl1.TabIndex = 12;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.pictureBox1);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.label7);
+            this.tabPage1.Controls.Add(this.groupBox5);
+            this.tabPage1.Controls.Add(this.groupBox4);
+            this.tabPage1.Controls.Add(this.groupBox3);
+            this.tabPage1.Controls.Add(this.groupBox1);
+            this.tabPage1.Location = new System.Drawing.Point(10, 48);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(950, 723);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Create Profile";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(302, 26);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(370, 53);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 28;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(330, 558);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(260, 60);
+            this.button2.TabIndex = 27;
+            this.button2.Text = "Create user";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(370, 118);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(326, 38);
+            this.textBox1.TabIndex = 25;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(259, 120);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(98, 32);
+            this.label7.TabIndex = 24;
+            this.label7.Text = "Name:";
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.radioButton9);
+            this.groupBox5.Controls.Add(this.radioButton10);
+            this.groupBox5.Location = new System.Drawing.Point(482, 389);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(370, 140);
+            this.groupBox5.TabIndex = 22;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Do you have children?";
+            // 
+            // radioButton9
+            // 
+            this.radioButton9.AutoSize = true;
+            this.radioButton9.Location = new System.Drawing.Point(218, 48);
+            this.radioButton9.Name = "radioButton9";
+            this.radioButton9.Size = new System.Drawing.Size(88, 36);
+            this.radioButton9.TabIndex = 8;
+            this.radioButton9.TabStop = true;
+            this.radioButton9.Text = "No";
+            this.radioButton9.UseVisualStyleBackColor = true;
+            // 
+            // radioButton10
+            // 
+            this.radioButton10.AutoSize = true;
+            this.radioButton10.Location = new System.Drawing.Point(49, 48);
+            this.radioButton10.Name = "radioButton10";
+            this.radioButton10.Size = new System.Drawing.Size(101, 36);
+            this.radioButton10.TabIndex = 7;
+            this.radioButton10.TabStop = true;
+            this.radioButton10.Text = "Yes";
+            this.radioButton10.UseVisualStyleBackColor = true;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.radioButton7);
+            this.groupBox4.Controls.Add(this.radioButton8);
+            this.groupBox4.Location = new System.Drawing.Point(84, 389);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(370, 140);
+            this.groupBox4.TabIndex = 21;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Are you a drinker?";
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.Location = new System.Drawing.Point(218, 48);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(88, 36);
+            this.radioButton7.TabIndex = 8;
+            this.radioButton7.TabStop = true;
+            this.radioButton7.Text = "No";
+            this.radioButton7.UseVisualStyleBackColor = true;
+            // 
+            // radioButton8
+            // 
+            this.radioButton8.AutoSize = true;
+            this.radioButton8.Location = new System.Drawing.Point(49, 48);
+            this.radioButton8.Name = "radioButton8";
+            this.radioButton8.Size = new System.Drawing.Size(101, 36);
+            this.radioButton8.TabIndex = 7;
+            this.radioButton8.TabStop = true;
+            this.radioButton8.Text = "Yes";
+            this.radioButton8.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.radioButton5);
+            this.groupBox3.Controls.Add(this.radioButton6);
+            this.groupBox3.Location = new System.Drawing.Point(84, 218);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(370, 140);
+            this.groupBox3.TabIndex = 20;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Are you a sleeper?";
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(218, 48);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(88, 36);
+            this.radioButton5.TabIndex = 8;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "No";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(49, 48);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(101, 36);
+            this.radioButton6.TabIndex = 7;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "Yes";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(482, 218);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(370, 140);
+            this.groupBox1.TabIndex = 18;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Are you a talker?";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(218, 48);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(88, 36);
+            this.radioButton2.TabIndex = 8;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "No";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(49, 48);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(101, 36);
+            this.radioButton1.TabIndex = 7;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Yes";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(10, 48);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(950, 723);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Choose Your Seat";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.dataGridView1);
+            this.tabPage3.Location = new System.Drawing.Point(10, 48);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(950, 723);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Delta View";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 40;
+            this.dataGridView1.Size = new System.Drawing.Size(956, 694);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(995, 837);
+            this.Controls.Add(this.tabControl1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Delta Profiler";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox5.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.RadioButton radioButton9;
+        private System.Windows.Forms.RadioButton radioButton10;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.RadioButton radioButton7;
+        private System.Windows.Forms.RadioButton radioButton8;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ DeltaProfile/Form1.cs
@@ -0,0 +1,313 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DeltaProfile
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            passengers.Columns.Add("Passenger");
+            passengers.Columns.Add("Seat");
+            passengers.Columns.Add("Talker?");
+            passengers.Columns.Add("Sleeper?");
+            passengers.Columns.Add("Drinker?");
+            passengers.Columns.Add("Child");
+
+            createSeats();
+            createUsers();
+        }
+
+        List<User> users = new List<User>();
+        SeatButton[] seatButtons = new SeatButton[30];
+        DataTable passengers = new DataTable();
+        List<int> neighbors = new List<int>();
+        double[] matchArr = new double[30];
+
+        protected void button_Click(object sender, EventArgs e)
+        {
+            SeatButton b = sender as SeatButton;
+            if (!b.occupied)
+            {
+                users[users.Count - 1].selectSeat(b.seatNumber + 1);
+                b.BackColor = Color.LightBlue;
+                b.occupySeat(users[users.Count - 1]);
+                b.occupied = true;
+                addPassengers(users[users.Count - 1]);
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            createSeats();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            User u = new User(textBox1.Text, radioButton1.Checked, radioButton6.Checked, radioButton8.Checked, radioButton10.Checked);
+            users.Add(u);
+            updateSeats(u);
+            textBox1.Text = "";
+            radioButton1.Checked = false;
+            radioButton2.Checked = false;
+            radioButton5.Checked = false;
+            radioButton6.Checked = false;
+            radioButton7.Checked = false;
+            radioButton8.Checked = false;
+            radioButton9.Checked = false;
+            radioButton10.Checked = false;
+        }
+        
+        public void addPassengers(User u)
+        {
+            if (!users.Contains(u)) { users.Add(u); }
+            passengers.Rows.Add(u.name, u.chosenSeat, boolToString(u.isTalker), boolToString(u.isSleeper), boolToString(u.isDrinker), boolToString(u.hasChildren));
+            dataGridView1.DataSource = passengers;
+            dataGridView1.Columns[0].Width = 75;
+            dataGridView1.Columns[1].Width = 40;
+            dataGridView1.Columns[2].Width = 50;
+            dataGridView1.Columns[3].Width = 50;
+            dataGridView1.Columns[4].Width = 50;
+            dataGridView1.Columns[5].Width = 50;
+        }
+
+        public void setUser(int s, User u)
+        {
+            seatButtons[s - 1].user = u;
+            seatButtons[s - 1].occupied = true;
+            seatButtons[s - 1].seatNumber = s;
+            u.seatButton = seatButtons[s - 1];
+        }
+
+        public void updateSeats(User u)
+        {
+            double matchNum = 0;
+            foreach (SeatButton sb in seatButtons)
+            {
+                if (sb.user != null)
+                {
+                    sb.BackColor = Color.Gray;
+                    sb.Enabled = false;
+                    matchArr[sb.seatNumber - 1] = -1;
+                }
+                else
+                {
+                    if (sb.seatNumber % 6 == 1)
+                    {
+                        if (seatButtons[sb.seatNumber].user != null)
+                        {
+                            matchArr[sb.seatNumber - 1] = matchUsers(u, seatButtons[sb.seatNumber].user);
+                        }
+                        else
+                        {
+                            matchArr[sb.seatNumber - 1] = 1;
+                        }
+                    }
+                    else if (sb.seatNumber % 6 == 0)
+                    {
+                        if (seatButtons[sb.seatNumber - 2].user != null)
+                        {
+                            matchArr[sb.seatNumber - 1] = matchUsers(u, seatButtons[sb.seatNumber - 2].user);
+                        }
+                        else
+                        {
+                            matchArr[sb.seatNumber - 1] = 1;
+                        }
+                    }
+                    else
+                    {
+                        if (seatButtons[sb.seatNumber].user != null)
+                        {
+                            if (seatButtons[sb.seatNumber - 2].user != null)
+                            {
+                                matchArr[sb.seatNumber - 1] = 0.5*(matchUsers(u, seatButtons[sb.seatNumber - 2].user) + matchUsers(u, seatButtons[sb.seatNumber].user));
+                            }
+                            else
+                            {
+                                matchArr[sb.seatNumber - 1] = matchUsers(u, seatButtons[sb.seatNumber].user);
+                            }
+                        }
+                        else if (seatButtons[sb.seatNumber- 2].user !=null)
+                        {
+                            matchArr[sb.seatNumber - 1] = matchUsers(u, seatButtons[sb.seatNumber - 2].user);
+                        }
+                        else
+                        {
+                            matchArr[sb.seatNumber - 1] = 1;
+                        }
+                    }
+                }
+            }
+            for (int i = 0; i < matchArr.Length; i++)
+            {
+                matchNum = matchArr[i];
+                if (matchNum >= 0 && matchNum < 0.25)
+                {
+                    seatButtons[i].BackColor = Color.Red;
+                }
+                else if (matchNum >= 0.25 && matchNum < 0.5)
+                {
+                    seatButtons[i].BackColor = Color.Pink;
+                }
+                else if (matchNum >= 0.5 && matchNum < 0.75)
+                {
+                    seatButtons[i].BackColor = Color.LightGreen;
+                }
+                else if (matchNum >= 0.75 && matchNum <= 1)
+                {
+                    seatButtons[i].BackColor = Color.Green;
+                }
+                else
+                {
+                    seatButtons[i].BackColor = Color.DarkGray;
+                    seatButtons[i].Enabled = false;
+                }
+                ToolTip tt = new ToolTip();
+                tt.SetToolTip(seatButtons[i], "Seat #" + seatButtons[i].Name + "\nMatch = " + 100 * matchNum + "%");
+            }   
+        }
+        
+        public double matchUsers(User u1, User u2)
+        {
+            double match = 0;
+            if (u1.isTalker == u2.isTalker) { match++; }
+            if (u1.isSleeper == u2.isSleeper) { match++; }
+            if (u1.isDrinker == u2.isDrinker) { match++; }
+            if (u1.hasChildren == u2.hasChildren) { match++; }
+            return match / 4;
+        }
+
+        public string boolToString(bool b)
+        {
+            if (b) { return "yes"; }
+            else { return "no"; }
+        }
+
+        public void createSeats()
+        {
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    SeatButton b = new SeatButton(false, i + j * 6 + 1, new Point((i * (tabPage2.Width - 10)) / 6, 8 + (((j * tabPage2.Height) - 10) / 7)));
+                    b.Name = (i + j * 6 + 1).ToString();
+                    b.Click += new EventHandler(button_Click);
+                    b.Text = b.Name;
+                    ToolTip tt = new ToolTip();
+                    tt.SetToolTip(b, "Seat #" + b.Name);
+                    tabPage2.Controls.Add(b);
+                    seatButtons[i + j * 6] = b;
+                }
+
+            }
+
+            for (int i = 3; i < 6; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    SeatButton b = new SeatButton(false, i + j * 6 + 1, new Point(((210 + i * (tabPage2.Width - 10)) / 6), 8 + (((j * tabPage2.Height) - 10) / 7)));
+                    b.Name = (i + j * 6 + 1).ToString();
+                    b.Click += new EventHandler(button_Click);
+                    b.Text = b.Name;
+                    ToolTip tt = new ToolTip();
+                    tt.SetToolTip(b, "Seat #" + b.Name);
+                    tabPage2.Controls.Add(b);
+                    seatButtons[i + j * 6] = b;
+                }
+
+            }
+        }
+
+        public void createUsers()
+        {
+            User u1 = new User("Dick", true, true, false, false, 1);
+            User u2 = new User("Tucker", true, true, false, false, 6);
+            User u3 = new User("Neil", false, false, false, false, 8);
+            User u4 = new User("Alon", true, true, true, true, 20);
+            addPassengers(u1);
+            addPassengers(u2);
+            addPassengers(u3);
+            addPassengers(u4);
+            setUser(u1.chosenSeat, u1);
+            setUser(u2.chosenSeat, u2);
+            setUser(u3.chosenSeat, u3);
+            setUser(u4.chosenSeat, u4);
+        }
+
+    }
+
+    public class SeatButton : Button
+    {
+        public bool occupied;
+        public int seatNumber;
+        public User user;
+
+        public SeatButton(bool o, int s, Point l)
+        {
+            occupied = o;
+            seatNumber = s;
+            Size = new Size(30, 30);
+            Location = l;
+        }
+
+        public SeatButton(bool o, int s, Point l, User u)
+        {
+            occupied = o;
+            seatNumber = s;
+            user = u;
+            Location = l;
+            Size = new Size(30, 30);
+        }
+
+        public void occupySeat(User u)
+        {
+            user = u;
+        }
+    }
+
+    public class User
+    {
+        public string name;
+        public bool isTalker;
+        public bool isSleeper;
+        public bool isDrinker;
+        public bool hasChildren;
+        public int chosenSeat;
+        public SeatButton seatButton;
+
+        public User(string n, bool t, bool s, bool d, bool c)
+        {
+            name = n;
+            isTalker = t;
+            isSleeper = s;
+            isDrinker = d;
+            hasChildren = c;
+            chosenSeat = -1;
+        }
+
+        public User(string n, bool t, bool s, bool d, bool c, int cs)
+        {
+            name = n;
+            isTalker = t;
+            isSleeper = s;
+            isDrinker = d;
+            hasChildren = c;
+            chosenSeat = 0;
+            chosenSeat = cs;
+        }
+
+        public void selectSeat(int s)
+        {
+            chosenSeat = s;
+        }
+    }
+}
